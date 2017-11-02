a2cf64146ff8ee91bb26c6fabad0a372c87797c3
tempo13 <breakthebias@naver.com>
2017-11-02 11:19:56 +0900

    url-html only connect one database

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 
     url(r'^$', views.blog_data, name='blog_data'),
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/$', views.post_list, name='post_list'),
 
     #11/2 url의 쓰인 순서에 따라 post_list.html에 출력되는게 다름
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,5 +12,6 @@ def post_list(request):
 
 def blog_data(request):
     parser_data = Blog_data.objects.filter(text__contains="[인재개발원]")
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'parser':parser_data})
 
